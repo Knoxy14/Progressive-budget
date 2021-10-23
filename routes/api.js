c2da98826f63518452c66a233090d4ea9eaf7cb3
derek knox <derekknox14@gmail.com>
2021-10-23 00:51:41 -0700

    icon and api js

---
+++ routes/api.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const Transaction = require("../models/transaction.js");
+const path = require("path");
+
+router.post("/api/transaction", ({body}, res) => {
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+router.post("/api/transaction/bulk", ({body}, res) => {
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+router.get("/api/transaction", (req, res) => {
+  Transaction.find({}).sort({date: -1})
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+router.get("/", function(req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
+});
+
+module.exports = router;
